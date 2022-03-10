3d441a66cacfdb2821cf17dc0dfc39f769f82380
Adrian Bratu <adrian.bratu@amrock.com>
2022-03-10 09:42:17 -0500

    packaging styles

--- projects/bra2/theme/src/lib/theme.module.ts
+++ projects/bra2/theme/src/lib/theme.module.ts
@@ -1,18 +1,4 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
-import { Bra2ThemeConfig, Bra2ThemeConfigService, Brat2ThemeService } from './services/theme.service';
+import { NgModule } from '@angular/core';
 
 @NgModule()
-export class ThemeModule { 
-  static forRoot(config: Bra2ThemeConfig): ModuleWithProviders<ThemeModule> {
-    return {
-      ngModule: ThemeModule,
-      providers: [
-        Brat2ThemeService,
-        {
-          provide: Bra2ThemeConfigService,
-          useValue: config
-        }
-      ]
-    }
-  }
-}
+export class ThemeModule {}
--- projects/bra2/theme/src/public-api.ts
+++ projects/bra2/theme/src/public-api.ts
@@ -1,6 +1,1 @@
-/*
- * Public API Surface of theme
- */
-
-export * from './lib/services/theme.service';
 export * from './lib/theme.module';
